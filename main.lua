a72eb65a68b4318070bffde2efca49aa3fde2b0e
HusseinMansourIQ <hussein.mansour132.2000@gmail.com>
2025-09-13 11:55:52 +0300

    initial

---
+++ main.lua
@@ -0,0 +1,225 @@
+refArray = {
+    { x = 365.585, y = 269.012 },
+    { x = 369.646, y = 265.512 },
+    { x = 373.065, y = 257.512 },
+    { x = 373.065, y = 247.013 },
+    { x = 377.767, y = 236.513 },
+    { x = 379.691, y = 221.513 },
+    { x = 381.4, y = 201.014 },
+    { x = 382.683, y = 188.014 },
+    { x = 381.708, y = 174.403 },
+    { x = 370.715, y = 157.515 },
+    { x = 366.868, y = 166.515 },
+    { x = 366.226, y = 191.014 },
+    { x = 364.089, y = 211.014 },
+    { x = 362.166, y = 214.014 },
+    { x = 358.319, y = 205.014 },
+    { x = 354.258, y = 196.014 },
+    { x = 354.044, y = 201.514 },
+    { x = 354.258, y = 209.014 },
+    { x = 354.899, y = 219.513 },
+    { x = 359.387, y = 214.014 },
+    { x = 361.311, y = 216.514 },
+    { x = 360.883, y = 228.013 },
+    { x = 360.029, y = 239.513 },
+    { x = 359.601, y = 257.012 },
+    { x = 362.593, y = 258.512 },
+    { x = 363.662, y = 267.512 },
+    { x = 365.585, y = 269.012 }
+}
+
+userArray = {
+    { x = 363.2166748046875, y = 261.9166717529297 },
+    { x = 363.2166748046875, y = 259.0833282470703 },
+    { x = 363.2166748046875, y = 250.5833282470703 },
+    { x = 361.79998779296875, y = 244.9166717529297 },
+    { x = 360.38330078125, y = 237.8333282470703 },
+    { x = 360.38330078125, y = 232.1666717529297 },
+    { x = 358.9666748046875, y = 226.49998474121094 },
+    { x = 357.54998779296875, y = 225.0833282470703 },
+    { x = 357.54998779296875, y = 223.6666717529297 },
+    { x = 357.54998779296875, y = 220.8333282470703 },
+    { x = 357.54998779296875, y = 219.4166717529297 },
+    { x = 357.54998779296875, y = 217.99998474121094 },
+    { x = 357.54998779296875, y = 213.74998474121094 },
+    { x = 357.54998779296875, y = 212.3333282470703 },
+    { x = 357.54998779296875, y = 210.9166717529297 },
+    { x = 357.54998779296875, y = 208.0833282470703 },
+    { x = 357.54998779296875, y = 205.24998474121094 },
+    { x = 357.54998779296875, y = 203.8333282470703 },
+    { x = 358.9666748046875, y = 200.99998474121094 },
+    { x = 358.9666748046875, y = 198.1666717529297 },
+    { x = 360.38330078125, y = 192.49998474121094 },
+    { x = 360.38330078125, y = 191.0833282470703 },
+    { x = 360.38330078125, y = 189.6666717529297 },
+    { x = 361.79998779296875, y = 185.4166717529297 },
+    { x = 363.2166748046875, y = 183.99998474121094 },
+    { x = 364.63330078125, y = 181.1666717529297 },
+    { x = 364.63330078125, y = 179.74998474121094 },
+    { x = 366.04998779296875, y = 176.9166717529297 },
+    { x = 367.4666748046875, y = 175.49998474121094 },
+    { x = 368.88330078125, y = 175.49998474121094 },
+    { x = 368.88330078125, y = 175.49998474121094 },
+    { x = 370.29998779296875, y = 175.49998474121094 },
+    { x = 370.29998779296875, y = 175.49998474121094 },
+    { x = 371.7166748046875, y = 175.49998474121094 },
+    { x = 373.13330078125, y = 175.49998474121094 },
+    { x = 375.9666748046875, y = 176.9166717529297 },
+    { x = 377.38330078125, y = 179.74998474121094 },
+    { x = 377.38330078125, y = 181.1666717529297 },
+    { x = 377.38330078125, y = 182.5833282470703 },
+    { x = 377.38330078125, y = 185.4166717529297 },
+    { x = 377.38330078125, y = 186.8333282470703 },
+    { x = 377.38330078125, y = 189.6666717529297 },
+    { x = 378.79998779296875, y = 192.49998474121094 },
+    { x = 378.79998779296875, y = 193.9166717529297 },
+    { x = 378.79998779296875, y = 196.74998474121094 },
+    { x = 378.79998779296875, y = 199.5833282470703 },
+    { x = 378.79998779296875, y = 202.4166717529297 },
+    { x = 378.79998779296875, y = 205.24998474121094 },
+    { x = 378.79998779296875, y = 209.49998474121094 },
+    { x = 378.79998779296875, y = 212.3333282470703 },
+    { x = 378.79998779296875, y = 216.5833282470703 },
+    { x = 377.38330078125, y = 222.24998474121094 },
+    { x = 375.9666748046875, y = 223.6666717529297 },
+    { x = 375.9666748046875, y = 226.49998474121094 },
+    { x = 375.9666748046875, y = 227.9166717529297 },
+    { x = 375.9666748046875, y = 229.3333282470703 },
+    { x = 375.9666748046875, y = 232.1666717529297 },
+    { x = 375.9666748046875, y = 234.99998474121094 },
+    { x = 375.9666748046875, y = 237.8333282470703 },
+    { x = 375.9666748046875, y = 240.6666717529297 },
+    { x = 375.9666748046875, y = 242.0833282470703 },
+    { x = 375.9666748046875, y = 243.49998474121094 },
+    { x = 375.9666748046875, y = 246.3333282470703 },
+    { x = 375.9666748046875, y = 249.1666717529297 },
+    { x = 375.9666748046875, y = 250.5833282470703 },
+    { x = 375.9666748046875, y = 251.99998474121094 },
+    { x = 375.9666748046875, y = 254.8333282470703 },
+    { x = 375.9666748046875, y = 256.24998474121094 },
+    { x = 375.9666748046875, y = 259.0833282470703 },
+    { x = 375.9666748046875, y = 261.9166717529297 },
+    { x = 375.9666748046875, y = 263.3333282470703 },
+    { x = 375.9666748046875, y = 264.74998474121094 },
+    { x = 374.54998779296875, y = 267.5833282470703 },
+    { x = 374.54998779296875, y = 268.99998474121094 },
+    { x = 373.13330078125, y = 271.8333282470703 },
+    { x = 373.13330078125, y = 273.24998474121094 },
+    { x = 371.7166748046875, y = 274.6666717529297 },
+    { x = 370.29998779296875, y = 276.0833282470703 },
+    { x = 368.88330078125, y = 276.0833282470703 },
+    { x = 367.4666748046875, y = 276.0833282470703 },
+    { x = 367.4666748046875, y = 276.0833282470703 },
+    { x = 366.04998779296875, y = 276.0833282470703 },
+    { x = 364.63330078125, y = 276.0833282470703 },
+    { x = 363.2166748046875, y = 274.6666717529297 }
+  }
+
+
+
+function dist(a, b)
+  return math.sqrt((a.x - b.x) ^ 2 + (a.y - b.y) ^ 2)
+end
+
+function pathLength(array)
+  local length = 0
+  for i = 1, #array - 1 do
+    length = length + dist(array[i], array[i + 1])
+  end
+  return length
+end
+
+
+-- this function to normilize the path to a fixed number of points
+function resamplePath(points, numPoints)
+    local pathLength = pathLength(points)
+    local segLength = pathLength / (numPoints)
+    local newPoints = {points[1]}
+
+    local D = 0 --carried distance 
+    for i = 1, #points - 1 do -- we do -1 because we don't want to go out of bounds with the last i+1
+        local p1 = points[i]
+        local p2 = points[i + 1]
+        local d = dist(p1, p2)
+
+        while D + d >= segLength do
+
+            -- linear interpolation to find the new point
+            local t = (segLength - D) / d
+            local nx = p1.x + (p2.x - p1.x) * t
+            local ny = p1.y + (p2.y - p1.y) * t
+            
+            local newPoint = {x = nx, y = ny}
+            table.insert(newPoints, newPoint)
+            p1 = newPoint 
+            d = dist(p1, p2) -- storing the distance from the new point to the point we were trying to reach to be added as an accumulator
+
+            D = 0
+
+        end
+        D = D + d 
+    end
+
+    while #newPoints < numPoints do --ensuring we have not less than the number of points we want
+        table.insert(newPoints, points[#points])
+    end
+
+    while #newPoints > numPoints do --ensuring we have not more than the number of points we want
+        table.remove(newPoints, #newPoints)
+    end
+    return newPoints
+end
+
+
+function chamferDistance(array1, array2)
+    local minDistance = math.huge
+    local avgDistance_1 = 0
+    local avgDistance_2 = 0
+
+    for i =1, #array1 do
+        local p1 = {x = array1[i].x, y = array1[i].y}
+        for j = 1, #array2 do
+            local p2 = {x = array2[j].x, y = array2[j].y}
+            local d = dist(p1, p2)
+            if d < minDistance then
+                minDistance = d
+            end
+        end
+        avgDistance_1 = avgDistance_1 + minDistance
+    end
+
+    for i = 1 , #array2 do
+        local p1 = {x = array2[i].x, y = array2[i].y}
+        for j = 1, #array1 do
+            local p2 = {x = array1[j].x, y = array1[j].y}
+            local d = dist(p1, p2)
+            if d < minDistance then
+                minDistance = d
+            end
+        end
+        avgDistance_2 = avgDistance_2 + minDistance
+    end
+
+
+    return (avgDistance_1/#array1 + avgDistance_2/#array2)/2
+end
+
+
+function distanceToPercent (avgDistance)
+    local maxAcceptableDistance = 40 -- we should find a better way to calculate this 
+    local pct = math.max(0, 100 * (1 - avgDistance / maxAcceptableDistance))
+    return math.floor(pct + 0.5)
+end
+
+
+function comparePaths (refArray, userArray)
+    local resampeledUserArray = resamplePath(userArray, 10)
+    local resampeledRefArray = resamplePath(refArray, 10)
+    local avgDistance = chamferDistance(resampeledUserArray, resampeledRefArray)
+    print(avgDistance)
+    local pct = distanceToPercent(avgDistance)
+    return pct
+end
+
+
+print(comparePaths(refArray, userArray))
